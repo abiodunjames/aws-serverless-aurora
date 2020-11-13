d2e27ce0680a05b6888bd36f8554ec27288c7739
samueljames <samuel4abiodun@gmail.com>
2020-11-13 16:43:31 +0100

    Clean up function

--- functions/get_posts.py
+++ functions/get_posts.py
@@ -4,11 +4,3 @@ from utils import execute_statement
 def handler(event, context):
     response = execute_statement(f"select * from posts")
     return {"statusCode": 200, "body": response["records"]}
-
-
-routes = {
-    "posts": [
-        {"method": "GET", "function": "get_posts"},
-        {"method": "POST", "function": "create_posts"},
-    ],
-}
